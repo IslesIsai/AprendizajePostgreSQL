8103b78e6a87633f18376b92b349b09a82aa1ed0
IslesIsai <Islesisai@gmail.com>
2025-11-26 11:49:45 -0400

    Creando apartado de editar

--- routes/clientes.js
+++ routes/clientes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../db');
-const { route } = require('./auth');
-
+ 
 function auth(req,res,next){
     if(!req.session.user) return res.redirect('/login');
     next();
@@ -10,7 +9,7 @@ function auth(req,res,next){
 
 router.get('/',auth,async(req,res)=>{
     const result = await pool.query('SELECT * FROM clientes ORDER BY nombre ASC')
-    res.render('clientes/index.njk',{cliente:result.rows})
+    res.render('clientes/index.njk',{clientes:result.rows})
 });
 
 router.get('/agregar',auth,(req,res)=>{
@@ -25,7 +24,7 @@ router.post('/agregar',auth,async(req,res)=>{
 
 router.get('/editar/:id',auth,async(req,res)=>{
     const {id} = req.params
-    const {result} = await pool.query('Select * FROM clientes WHERE id = $1 LIMIT 1',[id]);
+    const result = await pool.query('Select * FROM clientes WHERE id = $1 LIMIT 1',[id]);
     if(result.rowCount === 0) return res.send('Cliente no existe');
 
     res.render('/clientes/editar.njk',{cliente:result.rows[0]})
@@ -36,14 +35,14 @@ router.post('/editar/:id',auth,async(req,res)=>{
     const {nombre,apellido,telefono,email} = res.body;
 
     await pool.query('UPDATE clientes SET nombre = $1, apellido = $2, telefono = $3,  email = $4 WHERE id = $5 ',[nombre,apellido,telefono,email,id]);
-    res.render('/clientes')
+    res.redirect('/clientes')
 })
 
 router.get('/elimnar/:id',auth,async(req,res)=>{
-    const {id} = req.params;
+    const {id} = req.params
     
     await pool.query('DELETE FROM clientes WHERE id = $1',[id]);
-    res.render('/clientes')
+    res.redirect('/clientes')
 })
 
 module.exports = router;
