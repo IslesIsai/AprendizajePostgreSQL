367b171a17027e795d57e63ac0ef7f530717174c
IslesIsai <Islesisai@gmail.com>
2025-11-26 12:20:57 -0400

    solucionando erroes en Editar

--- routes/clientes.js
+++ routes/clientes.js
@@ -27,18 +27,18 @@ router.get('/editar/:id',auth,async(req,res)=>{
     const result = await pool.query('Select * FROM clientes WHERE id = $1 LIMIT 1',[id]);
     if(result.rowCount === 0) return res.send('Cliente no existe');
 
-    res.render('/clientes/editar.njk',{cliente:result.rows[0]})
+    res.render('clientes/editar.njk',{cliente:result.rows[0]})
 });
 
 router.post('/editar/:id',auth,async(req,res)=>{
     const {id} = req.params;
-    const {nombre,apellido,telefono,email} = res.body;
+    const {nombre,apellido,telefono,email} = req.body;
 
     await pool.query('UPDATE clientes SET nombre = $1, apellido = $2, telefono = $3,  email = $4 WHERE id = $5 ',[nombre,apellido,telefono,email,id]);
     res.redirect('/clientes')
 })
 
-router.get('/elimnar/:id',auth,async(req,res)=>{
+router.get('/eliminar/:id',auth,async(req,res)=>{
     const {id} = req.params
     
     await pool.query('DELETE FROM clientes WHERE id = $1',[id]);
